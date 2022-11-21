399e4ecba6fbbc60cac6a763c74a68fa2336f170
EzilLize <115159321+EzilLize@users.noreply.github.com>
2022-11-21 12:21:22 +0300

    Add files via upload

---
+++ lesson 20/assets/js/main.js
@@ -0,0 +1,64 @@
+//Zadanie 1
+//function ok(){
+//    let zxc=setTimeout(ok,3000,scrollBy(0,500));
+//    clearTimeout(zxc,1000,1)
+//}
+
+//Zadanie 2
+//function check() {
+//     if ((window.innerWidth-document.documentElement.clientWidth)>0) { //Clientwidth проверяет вертикальную прокрутку в условии if
+//        alert('done');
+//    }
+//}
+
+//Zadanie 3
+
+//function buttn(){ //Функция кнопки подсчёта
+//    var x1=Number(document.getElementById('z1').value);
+//    var x2=Number(document.getElementById('z2').value);
+//    var x3=Number(document.getElementById('z3').value);
+//    var x4=Number(document.getElementById('z4').value);
+//    var x5=Number(document.getElementById('z5').value);
+//    var x6=Number(document.getElementById('z6').value);
+//    var result=x1+x2+x3+x4+x5+x6; //Очень трудная формула для сложения всех шести инпутов
+//    document.getElementById('result').innerHTML=result;
+//    var id=form.getAttribute(id);
+//    document.getElementById('id').innerHTML=id
+//}
+
+//Zadanie 4
+
+//Zadanie 5
+//function gg(){
+//    let ol=document.getElementById('elem');
+//    let li=document.createElement('li'); //делает ещё один li
+//    li.innerHTML="пункт" //Добавляет в конец текста li этот текст
+//    ol.appendChild(li);
+//}
+
+//Zadanie 6
+//function Link(i) {
+//    let a = document.getElementsByClassName('link')[i] 
+//    a.innerText += `(${a.href})` //вписывает href в круглых скобках
+//}
+
+//Zadanie 7
+//function kvadrat(i) {
+//let numb= document.getElementsByClassName('numb')[i]
+//numb.innerText = Number(numb.innerText) **2 //Возводит число в квадрат
+//}
+
+//Zadanie 8
+//function counter() {
+//    let btn = document.getElementById('buttnClicker') //находит кнопку через id
+//    btn.innerText =Number(btn.innerText)+1 //считывает каждое нажатие на кнопку
+//}
+
+//Zadanie 9
+//let list = document.getElementById('ul') 
+//function newli() {
+//    let elem = document.createElement('li') //Создаёт новый li
+//    elem.innerText = 'пункт'// добавляет новый пункт
+//    elem.className = 'li'
+//    list.appendChild(elem)
+//}
